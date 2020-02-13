c718e39b026e107f585be767ad46f056c1013daa
joseumbertomoreira <joseumbertomoreira@gmail.com>
2020-02-12 22:47:01 -0300

    First commit: Lenthulius' project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Dashboard from './Dashboard';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<Switch>
+			<Route exact path="/">
+				<App/>
+			</Route>
+			<Route path="/dashboard">
+				<Dashboard/>
+			</Route>
+		</Switch>
+	</Router>,
+	document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
