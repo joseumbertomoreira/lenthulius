ee97b5b284de01b10107030a21aee53334ed1add
joseumbertomoreira <joseumbertomoreira@gmail.com>
2020-02-16 01:56:14 -0300

    index.js was changed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Redirect, Switch, Route } from "react-router-dom";
 
-import Login from './components/Login'
-// import Dashboard from './components/Dashboard.js.js'
+import Login from './components/Login/Login'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
 	return (
 		<Router>
 			<Switch>
 				<Route path="/dashboard">
-					
+					<Navbar/>
 				</Route>
 				<Route path="/login">
 					<Login/>
