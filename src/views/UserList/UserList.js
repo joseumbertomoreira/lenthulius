140334dedcfb6e75362fc0c844c0e0edf1eef304
joseumbertomoreira <joseumbertomoreira@gmail.com>
2020-03-03 10:17:44 -0300

    A new restructuring for user form

--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -4,9 +4,7 @@ import Grid from '@material-ui/core/Grid';
 
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 
@@ -22,6 +20,10 @@ const useStyles = makeStyles(theme => ({
   },
   content: {
     marginTop: theme.spacing(2)
+  },
+  form: {
+    width: '75%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
   },
   typo: {
     marginTop: theme.spacing(2),
@@ -44,48 +46,127 @@ const UserList = () => {
       <Grid
         container
         spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '10vh' }}
+        direction="row"
+        alignContent="flex-start"
       >
-        <Grid item xs={3}>
+        <Grid
+          item
+          xs={12}
+        >
           <Typography className={classes.content} variant="h4" gutterBottom>
             Cadastro de Usuário
           </Typography>
-          <form noValidate autoComplete="off">
-            <FormControl className={classes.content} variant="outlined">
-              <InputLabel htmlFor="component-outlined">Nome</InputLabel>
-              <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
-            </FormControl >
-            <FormControl className={classes.content} variant="outlined">
-              <InputLabel htmlFor="component-outlined">Sobrenome</InputLabel>
-              <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
-            </FormControl>
-            <FormControl className={classes.content} variant="outlined">
-              <InputLabel htmlFor="component-outlined">E-mail</InputLabel>
-              <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
-            </FormControl>
-            <FormControl className={classes.content} variant="outlined">
-              <InputLabel htmlFor="component-outlined">Senha</InputLabel>
-              <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
-            </FormControl>
-          </form>
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.content}
-          >
-            Enviar
-          </Button>          
-        </Grid>   
-      </Grid> 
+        </Grid>
+      </Grid>
       <Divider
         className={classes.divider}
       />
-      <div className={classes.content}>
-        <UsersTable users={users} />
-      </div>
+      <Grid
+        item
+        xs={12}        
+      >
+        <Grid
+          container
+          direction="row"
+          alignContent="flex-start"
+          spacing={0}
+        >
+          <Grid
+            item
+            xs={6}
+          >
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="nome"
+                label="Nome"
+                name="nome"
+                autoComplete="nome"
+                autoFocus
+                //onChange={e => setUser(e.target.value)}
+              />
+              <br/>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="sobrenome"
+                label="Sobrenome"
+                name="sobrenome"
+                autoComplete="sobrenome"
+                autoFocus
+                //onChange={e => setUser(e.target.value)}
+              />
+            </form>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+          >
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                //onChange={e => setUser(e.target.value)}
+              />
+              <br/>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="senha"
+                label="Senha"
+                name="senha"
+                autoComplete="senha"
+                autoFocus
+                //onChange={e => setUser(e.target.value)}
+              />
+            </form>
+          </Grid>
+        </Grid>       
+      </Grid>
+      <Grid
+        item
+        xs={12}        
+      >
+        <Grid
+          container
+          direction="row"
+          alignContent="flex-end"
+          spacing={0}
+        >
+          <Grid
+            item
+            xs={2}
+          >
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              //disabled={!valForm()}
+            >
+              enviar
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider
+        className={classes.divider}
+      />
     </div>
   );
 };
